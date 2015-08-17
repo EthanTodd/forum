96685bda39cc44250a105ccff0a33edf775522ef
Ethan Schiavi <etschiavi@gmail.com>
2015-08-17 11:06:54 -0400

    commit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/", function(req, res){
 
 //FORUMS PAGE(INDEX)
 app.get("/forums", function(req, res){
-	// console.log("forums page fired");
+
 	var template = fs.readFileSync("./views/index.html", "utf8");
 	db.all("SELECT * FROM interests", function(err, rows){
 		if(err){
@@ -45,22 +45,17 @@ app.get("/forums", function(req, res){
 		  	res.send(rendered);
 		}
 	});
-	// $interests.on("click", function(event){
-	// console.log("square clicked");
-// });
 });
 
 
 //THREADS PAGE
 app.get("/forums/:id/threads", function(req, res){
 	var id = req.params.id;
-	// console.log("threads page fired");
+
   	var template = fs.readFileSync("./views/threads.html", "utf8");
   	db.all("SELECT * FROM threads WHERE interests_id =? ORDER BY votes DESC", id, function(err, rows){
   		if (err){
   			console.log(err);
-  		}else{
-  			console.log(rows);
   		}
 	  	var myObject = [{id: id}];
 		var rendered = ejs.render(template, {stuff: rows, id: myObject});
@@ -71,7 +66,7 @@ app.get("/forums/:id/threads", function(req, res){
 
 //NEW THREAD PAGE
 app.get("/forums/:id/threads/new", function(req, res){
-	// console.log("new thread page fired");
+
 	var forumId = [{id: req.params.id}];
   	var template = fs.readFileSync("./views/new.html", "utf8");
   	var rendered = ejs.render(template, {id: forumId});
@@ -81,7 +76,7 @@ app.get("/forums/:id/threads/new", function(req, res){
 
 //REPLY PAGE
 app.get("/forums/:id/threads/:id/edit", function(req, res){
-	// console.log("reply thread page fired");
+
   	var template = fs.readFileSync("./views/edit.html", "utf8");
   	res.send(template);
 });
@@ -89,7 +84,7 @@ app.get("/forums/:id/threads/:id/edit", function(req, res){
 
 //SEE ONE THREAD PAGE this is  ashow
 app.get("/forums/:forums_id/threads/:threads_id", function(req, res){
-	// console.log("view thread page fired");
+
 	var forumId = req.params.forums_id;
 	var threadId = req.params.threads_id;
 	var myReplies;
@@ -97,13 +92,10 @@ app.get("/forums/:forums_id/threads/:threads_id", function(req, res){
 		if (err){
 			console.log(err);
 		}else{
-			// console.log(row);
 			db.all("SELECT * FROM replies WHERE threads_id=?", threadId, function(err, rows){
 				if (err){
 					console.log(err);
 				}else{
-					// console.log("spitting out all replies for selected thread");
-					// console.log(rows);
 					myReplies = rows;
 				}
 			var myObject = [{id: forumId}];
@@ -118,7 +110,7 @@ app.get("/forums/:forums_id/threads/:threads_id", function(req, res){
 
 //ADD A REPLY/COMMENT PAGE LOGIC
 app.post("/forums/:forum_id/threads/:threads_id/replies", function(req, res){
-	// console.log("reply thread page logic fired");
+
 	var oldVotes = parseInt(req.body.currentVotes);
 	if (req.body.vote == 1){
 		db.run("UPDATE threads SET votes=? WHERE id=?",
@@ -126,7 +118,6 @@ app.post("/forums/:forum_id/threads/:threads_id/replies", function(req, res){
 			function(err){
 				if(err){
 					console.log(err);
-				}else{
 				}
 		});
 	}
@@ -135,8 +126,6 @@ app.post("/forums/:forum_id/threads/:threads_id/replies", function(req, res){
 		function(err){
 			if(err){
 				console.log(err);
-			}else{
-				console.log("new info inserted into replies");
 			}
 	});
 	res.redirect("/forums/" + req.params.forum_id + "/threads/" + req.params.threads_id)
@@ -145,15 +134,12 @@ app.post("/forums/:forum_id/threads/:threads_id/replies", function(req, res){
 
 //NEW THREADS PAGE LOGIC
 app.post("/forums/:id/threads", function(req, res){
-	// console.log("new thread logic fired");
-	// console.log(req.params.id);
+
 	db.run("INSERT INTO threads (title, votes, interests_id) VALUES (?,?,?)",
 		req.body.newThread, 0, req.params.id,
 		function(err){
 			if (err){
 				console.log(err);
-			}else{
-				console.log("new thread inserted");
 			}
 	});
 	res.redirect("/forums/" + req.params.id + "/threads")
