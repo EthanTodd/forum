3c3a817a5c7efa4d3cf8a3981bfb199c922b9b78
Ethan Schiavi <etschiavi@gmail.com>
2015-08-16 23:40:50 -0400

    almost done with CSS. still need to adjust photos on index screen

--- seed.sql
+++ seed.sql
@@ -1,46 +1,46 @@
 
 
-INSERT INTO interests (title, class) VALUES ('one pot meals', 'pure-u-1-5');
+INSERT INTO interests (title, class) VALUES ('one pot meals', 'pure-u-lg-1-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('If I have to wash another dish... What can I make that only requires 1 pot to wash!?', 0, 1);
 INSERT INTO threads (title, votes, interests_id) VALUES ('I have about 4 items in the cabinet. Will that make me a meal?', 0, 1);
 
-INSERT INTO interests (title, class) VALUES ('the closet', 'pure-u-2-5');
+INSERT INTO interests (title, class) VALUES ('the closet', 'pure-u-lg-2-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('OMG It is starting to pile up in there. Any tips for being ruthless??', 0, 2);
 INSERT INTO threads (title, votes, interests_id) VALUES ('I can not figure out where this smell is coming from', 0, 2);
 
-INSERT INTO interests (title, class) VALUES ('small kitchens', 'pure-u-1-5');
+INSERT INTO interests (title, class) VALUES ('small kitchens', 'pure-u-lg-1-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('I have one drawer in my entire kitchen LOL. Where the heck am I supposed to put everything else?', 0, 3);
 INSERT INTO threads (title, votes, interests_id) VALUES ('Anyone have a winning idea for getting my pots out of the cabinet? I need the space for extra shoes.', 0, 3);
 
-INSERT INTO interests (title, class) VALUES ('storage ideas', 'pure-u-1-5');
+INSERT INTO interests (title, class) VALUES ('storage ideas', 'pure-u-lg-1-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('So what is the deal with hiding all my crap? How does one make that happen fast and easy?', 0, 4);
 INSERT INTO threads (title, votes, interests_id) VALUES ('My new apartment does not have a storage space in the basement. Has anyone used a remote storage before?', 0, 4);
 
-INSERT INTO interests (title, class) VALUES ('entertaining', 'pure-u-2-5');
+INSERT INTO interests (title, class) VALUES ('entertaining', 'pure-u-lg-2-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('I guess my coffee table can double as a dining room table? Maybe just serve very small plates?', 0, 5);
 INSERT INTO threads (title, votes, interests_id) VALUES ('I want to have people over more often but I am just not sure how to be all Martha Stewart about it. Help!', 0, 5);
 
-INSERT INTO interests (title, class) VALUES ('indoor plants', 'pure-u-1-5');
+INSERT INTO interests (title, class) VALUES ('indoor plants', 'pure-u-lg-1-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('Not ready for a child, so let me see if I can keep a plant alive. Now... how does one do that?', 0, 6);
 INSERT INTO threads (title, votes, interests_id) VALUES ('PLANTS HATE ME.', 0, 6);
 
-INSERT INTO interests (title, class) VALUES ('when to clean', 'pure-u-2-5');
+INSERT INTO interests (title, class) VALUES ('when to clean', 'pure-u-lg-2-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('I am lazy and poor. I can not hire anyone to do it so how do I get this place clean fast and easy?', 0, 7);
 INSERT INTO threads (title, votes, interests_id) VALUES ('I just saw a really big bug. How do I make them go away ASAP?', 0, 7);
 
-INSERT INTO interests (title, class) VALUES ('buying food', 'pure-u-1-5');
+INSERT INTO interests (title, class) VALUES ('buying food', 'pure-u-lg-1-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('So, I am really good at letting all my food go bad. I just never know what to buy!', 0, 8);
 INSERT INTO threads (title, votes, interests_id) VALUES ('Has anyone used a food delivery service? I have heard they are good.', 0, 8);
 
-INSERT INTO interests (title, class) VALUES ('how to hang art', 'pure-u-1-5');
+INSERT INTO interests (title, class) VALUES ('how to hang art', 'pure-u-lg-1-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('If I just framed my black light poster can I keep it?', 0, 9);
 INSERT INTO threads (title, votes, interests_id) VALUES ('My place is tiny. What is the best way to arrange everything on the wall?', 0, 9);
 
-INSERT INTO interests (title, class) VALUES ('sharing the space', 'pure-u-2-5');
+INSERT INTO interests (title, class) VALUES ('sharing the space', 'pure-u-lg-2-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('I can not stand to look at my boyfriend anymore. I love him but he is always here. Has anyone else dealt with this?', 0, 10);
 INSERT INTO threads (title, votes, interests_id) VALUES ('I need a new roomie but how do I make sure I am making the right choice?', 0, 10);
 
-INSERT INTO interests (title, class) VALUES ('your best friend', 'pure-u-1-5');
+INSERT INTO interests (title, class) VALUES ('your best friend', 'pure-u-lg-1-5');
 INSERT INTO threads (title, votes, interests_id) VALUES ('I want a puppy! How much space do they need?', 0, 11);
 INSERT INTO threads (title, votes, interests_id) VALUES ('Any ideas on how to calm my dog down? He has so much energy when I get home from work. I take him for a walk but it is not enough', 0, 11);
 
